0fb8d22394e4e01ca22b0ee4dffb54883635d061
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-08-26 17:59:10 +0200

    solve exercise number 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeatNumber) {
+    let output = ""
+    for(let i=0; i<repeatNumber; i++){
+        output+=word
+    }
+    return output
 };
 
 // Do not edit below this line
