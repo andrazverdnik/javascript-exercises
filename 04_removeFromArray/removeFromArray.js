ba233522e4576fb7c0300242a347c1dacb7d6604
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-08-26 18:56:46 +0200

    solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let output = []
+    array.forEach(item => {
+        if (!args.includes(item)){
+            output.push(item);
+        }
+    })
+    return output;
 };
 
 // Do not edit below this line
