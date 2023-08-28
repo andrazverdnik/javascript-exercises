43bb76e3ef53a03c90ede63f870a720f2e080bc6
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-08-28 12:42:06 +0200

    solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(rangeMin, rangeMax) {
+    let output = 0;
+    for(let i = rangeMin; i<=rangeMax; i++){
+        output+=i;
+    }
+    return output;
 };
 
 // Do not edit below this line
