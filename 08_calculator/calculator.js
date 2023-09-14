16e4f36d2150b30b7aa49fa1d8fafd2bee0b30bd
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-09-14 15:52:20 +0200

    complete 08_calculator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,46 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a-b
 };
 
-const sum = function() {
-	
+const sum = function(input) {
+	let output = 0
+  for(let i = 0;i< input.length; i++){
+    output = add(output, input[i])
+  }
+  return output;
 };
 
-const multiply = function() {
-
+const multiply = function(input) {
+  let output = input[0]
+  for(let i = 1;i< input.length; i++){
+    output = output*input[i]
+  }
+  return output;
 };
 
-const power = function() {
-	
+const power = function(base, power) {
+	output = base
+  for(let i = 1; i<power; i++){
+    output = multiply([output, base])
+  }
+  return output;
 };
 
-const factorial = function() {
-	
+const factorial = function(input) {
+	let output = 1
+  if(!input==0){
+    for(i=0;i<input;i++){
+      output = (i+1)*output
+    }
+    return output
+  } else{
+    return 1;
+  }
+  
 };
 
 // Do not edit below this line
