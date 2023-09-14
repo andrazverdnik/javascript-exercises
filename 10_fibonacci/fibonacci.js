5015e0c432b343ec8530e714354b5af02a81b74c
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-09-14 22:19:16 +0200

    solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    let fibSequence = [0,1]
+    let i=2
+    if(input<0){
+        return "OOPS"
+    } else{
+        input = parseInt(input)
+        let length = input+2;
+        while(i<length){
+            fibSequence.push(fibSequence[i-2]+fibSequence[i-1])
+            i=i+1
+        }
+        return fibSequence[fibSequence.length-2]
+    }
+    
 
 };
 
