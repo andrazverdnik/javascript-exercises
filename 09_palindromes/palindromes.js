ad4b6c06a1206ffc5845e9e95da12843a873ab24
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-09-14 16:04:43 +0200

    complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.replace(/[^a-z0-9]/gi, '')
+    input = input.toLowerCase()
+    let reverse = ((input.split("")).reverse()).join("")
+    console.log(reverse)
+    if(reverse == input){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
