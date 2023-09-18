699c1dfd72925c93180681f355e1ae01a2f66f6f
Andraž Verdnik <grillbottoms@Andrazs-MacBook-Pro.local>
2023-09-18 21:04:23 +0200

    solve 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,15 @@
-const findTheOldest = function() {
+
+const findTheOldest = function(input) {
+    return input.reduce((oldest, current) =>{
+        const ageOfOldest = oldest.yearOfDeath - oldest.yearOfBirth;
+        const ageOfCurrent = current.yearOfDeath - current.yearOfBirth;
+
+        if(ageOfCurrent>ageOfOldest){
+            return current
+        }else{
+            return oldest
+        }
+    }, input[0])
 
 };
 
